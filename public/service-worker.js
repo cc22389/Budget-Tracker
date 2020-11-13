8cf046c556c01910428e8aa5b6117c5fc61d0781
Casey Morgan <caseyjeanmorgan@gmail.com>
2020-11-13 14:38:03 -0600

    deleted duplicate code in service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,10 +16,7 @@ const FILES_TO_CACHE = [
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
   ];
-  
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
-  
+    
   // install
   self.addEventListener("install", function(evt) {
     evt.waitUntil(

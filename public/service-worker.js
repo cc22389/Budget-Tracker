d1a8a416af5fc563b40013428df60af5952e7e41
Casey Morgan <caseyjeanmorgan@gmail.com>
2020-11-13 11:35:53 -0600

    upddated file names/pathways + added icon sizing code into service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,17 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const iconSizes = ["192", "512"];
+const iconFiles = iconSizes.map(
+  (size) => `/icons/icon-${size}x${size}.png`
+);
+
+
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/index.js",
     "/style.css",
-    "/assets/js/loadPosts.js",
     "/manifest.webmanifest",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
